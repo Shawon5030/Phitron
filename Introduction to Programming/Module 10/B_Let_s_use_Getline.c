b76eceea19226878680691b8b2b556d77e4d750c
Mahmudul Haque Shawon <haquemahmudul600@gmail.com>
2025-04-20 22:21:45 +0600

    Auto update on new file: E_Count.c

---
+++ Introduction to Programming/Module 10/B_Let_s_use_Getline.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+    char str[1000001];
+    fgets(str,1000001,stdin);
+    
+    int i=0;
+    while (str[i] !='\\' )
+    {
+        printf("%c",str[i]);
+        i++;
+    }
+    
+    return 0;
+}
