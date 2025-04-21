e9bad3578ea9ed4f7ee19aa3fe2f96edc0c4ad30
Mahmudul Haque Shawon <haquemahmudul600@gmail.com>
2025-04-21 17:18:33 +0600

    Auto update on new file: Multiply.c

---
+++ Introduction to Programming/Module 10/c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. /\\/\\/\\");
+    return 0;
+}
