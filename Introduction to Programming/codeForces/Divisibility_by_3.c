69820d8b065d6b62564192d7b03d753a47f6a9ef
Mahmudul Haque Shawon <haquemahmudul600@gmail.com>
2025-05-03 00:45:26 +0600

    Auto update on new file: Sir_Tripleston.c

---
+++ Introduction to Programming/codeForces/Divisibility_by_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    char arr[n];
+    scanf("%s",arr);
+    int sum=0;
+   for(int i=0;i<n;i++){
+    int a = arr[i]-'0';
+    sum+=a;
+   }
+    
+  
+
+    if(sum%3==0)printf("YES");
+    else{
+        printf("NO");
+    }
+    return 0;
+}
