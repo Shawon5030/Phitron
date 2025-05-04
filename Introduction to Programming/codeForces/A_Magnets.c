f604bd6cf3e3ae7663e23dba22da18dabc8565f7
Mahmudul Haque Shawon <149573785+Shawon5030@users.noreply.github.com>
2025-05-04 23:41:07 +0600

    Create A_Magnets.c

---
+++ Introduction to Programming/codeForces/A_Magnets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+    int t;
+    int a=t;
+    scanf("%d",&t);
+    int count=1;
+    int arr[t];
+    for(int i=0;i<t;i++)scanf("%d",&arr[i]);
+
+    for(int i=0;i<t-1;i++)
+    {
+        int f = arr[i]/10;
+        int s = arr[i+1]/10;
+
+        if(f!=s)count++;
+
+    }
+
+    printf("%d",count);
+    return 0;
+}
