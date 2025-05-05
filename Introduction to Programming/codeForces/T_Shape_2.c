0f91958bd1368e413e175e9af432de498e394871
Mahmudul Haque Shawon <haquemahmudul600@gmail.com>
2025-05-05 23:11:41 +0600

    Add new file: T_Shape_2.c

---
+++ Introduction to Programming/codeForces/T_Shape_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+
+    int space=n;
+    int star=1;
+    for(int i=1;i<=4;i++){
+
+        for(int j=space;j>0;j--){
+            printf(" ");
+        }
+        for(int k=star;k<0;k++){
+            printf("*");
+        }
+        star+=2;
+        space-=1;
+        
+    }
+    return 0;
+}
