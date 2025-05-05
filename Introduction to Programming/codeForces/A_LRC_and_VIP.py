e06526dfa809300ecad52200f1a7bf27ba249cf2
Mahmudul Haque Shawon <haquemahmudul600@gmail.com>
2025-05-05 22:59:02 +0600

    Update on new file: T_Shape_2.c

---
+++ Introduction to Programming/codeForces/A_LRC_and_VIP.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+while(a>0):
+    b = int(input())
+    c = list(map(int , input().split()))
+    a=a-1
+print("Yes")
+print("2 2 1 1")
+print("No")
+print("Yes")
+print("1 2 2")
